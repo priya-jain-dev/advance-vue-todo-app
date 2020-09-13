c51eea6661bb6606369b2be73805419bef345c01
priya jain <priya.jain@dingg.app>
2020-09-13 21:47:32 +0530

    manage tabs

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,7 +1,6 @@
 <template>
   <v-main>
     <v-container fluid>
-      
       <v-row align="center" justify="center">
         <v-col cols="12" sm="8" md="6" class="ma-5">
           <h1 class="todo-title">#todo</h1>
@@ -9,12 +8,12 @@
             <v-row>
               <v-col cols="8" class="ml-5">
                 <v-card-title class="card_title">
-                  <b>Sunday,</b> 13th
+                  <b>{{moment().format('MMMM, Do')}}</b>
                   <br />
                 </v-card-title>
                 <v-card-subtitle class="card-sub-title">
-                  <span>September</span>
-                </v-card-subtitle>  
+                  <span>{{moment().format('dddd')}}</span>
+                </v-card-subtitle>
               </v-col>
               <v-col class="mt-2">
                 <v-progress-circular
@@ -47,56 +46,145 @@
             </v-tabs>
             <v-divider></v-divider>
             <v-tabs-items v-model="tab">
-              <v-tab-item v-for="item in items" :key="item">
-                <v-card color="basil" flat>
-                  <v-text-field
-                    v-model="task"
-                    single-line
-                    solo
-                    :clear-icon="icons[0]"
-                    clearable
-                    label="Enter New Todo"
-                    type="text"
-                    @click:clear="clearMessage"
-                    @keydown.enter="create"
-                    hint="Press enter to create task"
-                    class="my-text-style"
-                  >
-                    <template v-slot:append>
-                      <v-btn depressed rounded color="secondary" class="ma-0" @click="show">Add</v-btn>
-                    </template>
-                  </v-text-field>
-                  <v-card v-if="tasks.length > 0">
-                    <v-slide-y-transition class="py-0" group tag="v-list">
-                      <template v-for="(task, i) in tasks">
-                        <v-divider v-if="i !== 0" :key="`${i}-divider`"></v-divider>
-
-                        <v-list-item :key="`${i}-${task.text}`">
-                          <v-list-item-action>
-                            <v-checkbox
-                              v-model="task.done"
-                              :color="task.done && 'accent' || 'secondary'"
-                              >
-                              <template v-slot:label>
-                                <div
-                                  :class="task.done && 'grey--text text-decoration-line-through' || 'black--text'"
-                                  class="ml-4"
-                                  v-text="task.text"
-                                ></div>
-                              </template>
-                            </v-checkbox>
-                          </v-list-item-action>
-
-                          <!-- <v-spacer></v-spacer> -->
-                          <!-- <v-scroll-x-transition>
-                            <v-icon v-if="task.done" color="">check</v-icon>
-                          </v-scroll-x-transition> -->
-                        </v-list-item>
+              <v-tab-item v-for="(item,index) in items" :key="item">
+                <!-- All -->
+                <template v-if="index == 0">
+                  <v-card color="basil" flat>
+                    <v-text-field
+                      v-model="task"
+                      single-line
+                      solo
+                      :clear-icon="icons[0]"
+                      clearable
+                      label="Enter New Todo"
+                      type="text"
+                      @click:clear="clearMessage"
+                      @keydown.enter="create"
+                      hint="Press enter to create task"
+                      class="my-text-style"
+                    >
+                      <template v-slot:append>
+                        <v-btn depressed rounded color="secondary" class="ma-0" @click="create">Add</v-btn>
                       </template>
-                    </v-slide-y-transition>
+                    </v-text-field>
+                    <v-card v-if="tasks.length > 0">
+                      <v-slide-y-transition class="py-0" group tag="v-list">
+                        <template v-for="(task, i) in tasks">
+                          <v-divider v-if="i !== 0" :key="`${i}-divider`"></v-divider>
+                          <v-list-item :key="`${i}-${task.text}`">
+                            <v-list-item-action>
+                              <v-checkbox
+                                v-model="task.done"
+                                :color="task.done && 'accent' || 'secondary'"
+                              >
+                                <template v-slot:label>
+                                  <div
+                                    :class="task.done && 'grey--text text-decoration-line-through' || 'black--text'"
+                                    class="ml-4"
+                                    v-text="task.text"
+                                  ></div>
+                                </template>
+                              </v-checkbox>
+                            </v-list-item-action>
+                          </v-list-item>
+                        </template>
+                      </v-slide-y-transition>
+                    </v-card>
                   </v-card>
-                  <!-- <v-card-text>{{ text }}</v-card-text> -->
-                </v-card>
+                </template>
+
+                <!-- Active -->
+                <template v-if="index == 1">
+                  <v-card color="basil" flat>
+                    <v-text-field
+                      v-model="task"
+                      single-line
+                      solo
+                      :clear-icon="icons[0]"
+                      clearable
+                      label="Enter New Todo"
+                      type="text"
+                      @click:clear="clearMessage"
+                      @keydown.enter="create"
+                      hint="Press enter to create task"
+                      class="my-text-style"
+                    >
+                      <template v-slot:append>
+                        <v-btn depressed rounded color="secondary" class="ma-0" @click="create">Add</v-btn>
+                      </template>
+                    </v-text-field>
+                    <v-card v-if="remainingTasks">
+                      <v-slide-y-transition class="py-0" group tag="v-list">
+                        <template v-for="(task, i) in tasks">
+                          <template v-if="!task.done">
+                            <v-divider v-if="i !== 0" :key="`${i}-divider`"></v-divider>
+                            <v-list-item :key="`${i}-${task.text}`">
+                              <v-list-item-action>
+                                <v-checkbox
+                                  v-model="task.done"
+                                  :color="task.done && 'accent' || 'secondary'"
+                                >
+                                  <template v-slot:label>
+                                    <div
+                                      :class="task.done && 'grey--text text-decoration-line-through' || 'black--text'"
+                                      class="ml-4"
+                                      v-text="task.text"
+                                    ></div>
+                                  </template>
+                                </v-checkbox>
+                              </v-list-item-action>
+                            </v-list-item>
+                          </template>
+                        </template>
+                      </v-slide-y-transition>
+                    </v-card>
+                  </v-card>
+                </template>
+
+                <!-- Completed -->
+                <template v-if="index == 2">
+                  <v-card color="basil" flat>
+                    <v-card v-if="completedTasks">
+                      <v-slide-y-transition class="py-0" group tag="v-list">
+                        <template v-for="(task, i) in tasks">
+                          <template v-if="task.done">
+                            <v-divider v-if="i !== 0" :key="`${i}-divider`"></v-divider>
+                            <v-list-item :key="`${i}-${task.text}`">
+                              <v-list-item-action>
+                                <v-checkbox
+                                  v-model="task.done"
+                                  :color="task.done && 'accent' || 'secondary'"
+                                >
+                                  <template v-slot:label>
+                                    <div
+                                      :class="task.done && 'grey--text text-decoration-line-through' || 'black--text'"
+                                      class="ml-4"
+                                      v-text="task.text"
+                                    ></div>
+                                  </template>
+                                </v-checkbox>
+                              </v-list-item-action>
+                              <v-spacer></v-spacer>
+                              <v-scroll-x-transition>
+                                <v-btn text color="accent"  @click="deleteItem(i)">
+                                  <v-icon color="accent">delete_outline</v-icon>
+                                </v-btn>
+                              </v-scroll-x-transition>
+                            </v-list-item>
+                          </template>
+                        </template>
+                      </v-slide-y-transition>
+                      <v-btn
+                        color="secondary"
+                        class="ma-2 white--text float-right"
+                        @click="deleteAll"
+                        v-if="completedTasks"
+                      >
+                        <v-icon left>delete_outline</v-icon>delete all
+                      </v-btn>
+                    </v-card>
+                  </v-card>
+                </template>
               </v-tab-item>
             </v-tabs-items>
           </v-card>
@@ -108,11 +196,13 @@
 
 <script>
 import { mdiClose } from "@mdi/js";
+var moment = require("moment");
 
 export default {
   name: "Todo",
   data() {
     return {
+      moment: moment,
       tab: null,
       items: ["All", "Active", "Completed"],
       tasks: [
@@ -139,7 +229,7 @@ export default {
       return this.tasks.filter((task) => task.done).length;
     },
     progress() {
-      return (this.completedTasks / this.tasks.length) * 100;
+      return (this.completedTasks / this.tasks.length) * 100 ? (this.completedTasks / this.tasks.length) * 100 : 0;
     },
     remainingTasks() {
       return this.tasks.length - this.completedTasks;
@@ -158,6 +248,13 @@ export default {
     clearMessage() {
       this.message = "";
     },
+    deleteAll(){
+      const all_delete = this.tasks.filter((task) => task.done);
+      all_delete.forEach(f => this.tasks.splice(this.tasks.findIndex(e => e.text === f.text),1));
+    },
+    deleteItem(i){
+      this.tasks.splice(i);
+    }
   },
 };
 </script>
@@ -192,5 +289,4 @@ export default {
   letter-spacing: 0.4px;
   text-align: left;
 }
-
 </style>
